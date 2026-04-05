983780bcfb3f75c05d08644365d15a6d6decb504
Michal Kowalski <michalo19871@gmail.com>
2026-04-05 13:40:15 +0200

    fix

--- script/commit.sh
+++ script/commit.sh
@@ -3,11 +3,11 @@ rm -rf NotificationSystem/.git
 
 git fetch --unshallow
 
-git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/NotificationSystem/_git/NotificationSystem.git
+git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/_git/NotificationSystem
 
 git config --global user.name "$AZUSERNAME"
 git config --global user.email "$AZUSER_EMAIL"
 
 git add .
 git commit -m "Automated commit from Azure DevOps pipeline"
-git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/NotificationSystem/_git/NotificationSystem.git
+git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/_git/NotificationSystem
