e3fbecbc5ab8a19cf02c0c466714f8bdb6e45687
Michal Kowalski <michalo19871@gmail.com>
2026-04-05 13:10:57 +0200

    sync github with ado

---
+++ script/commit.sh
@@ -0,0 +1,17 @@
+AZUREPAT = $AZUREPAT
+AZUSERNAME = $AZUSERNAME
+AZUSER_EMAIL = $AZUSER_EMAIL
+AZORG = $AZORG
+
+rm -rf NotificationSystem/.git
+
+git fetch --unshallow
+
+git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/NotificationSystem/_git/NotificationSystem.git
+
+git config --global user.name "$AZUSERNAME"
+git config --global user.email "$AZUSER_EMAIL"
+
+git add .
+git commit -m "Automated commit from Azure DevOps pipeline"
+git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/NotificationSystem/_git/NotificationSystem.git
